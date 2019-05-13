a647a280fefddf63148a1293e483b01e0f256f4c
Matthew Beck <MatthewAlexanderBeck@users.noreply.github.com>
2019-05-13 12:01:00 +0100

    EE-535: formatted CYAH

--- app/utils/CheckYourAnswersHelper.scala
+++ app/utils/CheckYourAnswersHelper.scala
@@ -25,35 +25,42 @@ import viewmodels.AnswerRow
 class CheckYourAnswersHelper(userAnswers: UserAnswers)(implicit messages: Messages)  {
 
   def employerContribution: Option[AnswerRow] = userAnswers.get(EmployerContributionPage) map {
-    x => AnswerRow("employerContribution.checkYourAnswersLabel", if(x) "site.yes" else "site.no", true, routes.EmployerContributionController.onPageLoad(CheckMode).url)
+    x => AnswerRow(
+      label = "employerContribution.checkYourAnswersLabel",
+      answer = if (x) "site.yes" else "site.no",
+      answerIsMessageKey = true,
+      changeUrl = routes.EmployerContributionController.onPageLoad(CheckMode).url)
   }
 
   def yourEmployer: Option[AnswerRow] = (userAnswers.get(YourEmployerPage), userAnswers.get(YourEmployersNames)) match {
     case (Some(x), Some(employers)) =>
-      Some(AnswerRow("yourEmployer.checkYourAnswersLabel",
-        if (x) "site.yes" else "site.no",
+      Some(AnswerRow(
+        label = "yourEmployer.checkYourAnswersLabel",
+        answer = if (x) "site.yes" else "site.no",
         answerIsMessageKey = true,
-        routes.YourEmployerController.onPageLoad(CheckMode).url,
-        Employment.asLabel(employers)
+        changeUrl = routes.YourEmployerController.onPageLoad(CheckMode).url,
+        labelArgs = Employment.asLabel(employers)
       ))
     case _ => None
   }
 
   def yourAddress: Option[AnswerRow] = (userAnswers.get(YourAddressPage), userAnswers.get(CitizensDetailsAddress)) match {
     case (Some(x), Some(address)) =>
-      Some(AnswerRow("yourAddress.checkYourAnswersLabel",
-        if (x) "site.yes" else "site.no",
-        true,
-        routes.YourAddressController.onPageLoad(CheckMode).url,
-        Address.asString(address))
+      Some(AnswerRow(
+        label = "yourAddress.checkYourAnswersLabel",
+        answer = if (x) "site.yes" else "site.no",
+        answerIsMessageKey = true,
+        changeUrl = routes.YourAddressController.onPageLoad(CheckMode).url,
+        labelArgs = Address.asString(address))
       )
     case _ => None
   }
 
   def taxYearSelection: Option[AnswerRow] = userAnswers.get(TaxYearSelectionPage) map {
     taxYears =>
-      AnswerRow("taxYearSelection.checkYourAnswersLabel",
-        taxYears.map {
+      AnswerRow(
+        label = "taxYearSelection.checkYourAnswersLabel",
+        answer = taxYears.map {
           taxYear =>
             messages(s"taxYearSelection.$taxYear",
               TaxYearSelection.getTaxYear(taxYear).toString,
@@ -61,13 +68,16 @@ class CheckYourAnswersHelper(userAnswers: UserAnswers)(implicit messages: Messag
             )
         }.mkString("<br>"),
         answerIsMessageKey = false,
-        routes.TaxYearSelectionController.onPageLoad(CheckMode).url
+        changeUrl = routes.TaxYearSelectionController.onPageLoad(CheckMode).url
       )
   }
 
   def sameAmountAllYears: Option[AnswerRow] = userAnswers.get(SameAmountAllYearsPage) map {
-    x => AnswerRow("sameAmountAllYears.checkYourAnswersLabel", if(x) "site.yes" else "site.no", true,
-      routes.SameAmountAllYearsController.onPageLoad(CheckMode).url)
+    x => AnswerRow(
+      label = "sameAmountAllYears.checkYourAnswersLabel",
+      answer = if (x) "site.yes" else "site.no",
+      answerIsMessageKey = true,
+      changeUrl = routes.SameAmountAllYearsController.onPageLoad(CheckMode).url)
   }
 
   def subscriptionAmount: Option[AnswerRow] = userAnswers.get(SubscriptionAmountPage) map {
@@ -80,11 +90,18 @@ class CheckYourAnswersHelper(userAnswers: UserAnswers)(implicit messages: Messag
   }
 
   def expensesEmployerPaid: Option[AnswerRow] = userAnswers.get(ExpensesEmployerPaidPage) map {
-    x => AnswerRow("ExpensesEmployerPaid.checkYourAnswersLabel",s"$x", false, routes.ExpensesEmployerPaidController.onPageLoad(CheckMode).url)
+    x => AnswerRow(
+      label = "ExpensesEmployerPaid.checkYourAnswersLabel",
+      answer = s"$x",
+      answerIsMessageKey = false,
+      changeUrl = routes.ExpensesEmployerPaidController.onPageLoad(CheckMode).url)
   }
 
   def addAnotherSubscription: Option[AnswerRow] = userAnswers.get(AddAnotherSubscriptionPage) map {
-    x => AnswerRow("addAnotherSubscription.checkYourAnswersLabel", if(x) "site.yes" else "site.no", true,
-      routes.AddAnotherSubscriptionController.onPageLoad(CheckMode).url)
+    x => AnswerRow(
+      label = "addAnotherSubscription.checkYourAnswersLabel",
+      answer = if (x) "site.yes" else "site.no",
+      answerIsMessageKey = true,
+      changeUrl = routes.AddAnotherSubscriptionController.onPageLoad(CheckMode).url)
   }
 }
