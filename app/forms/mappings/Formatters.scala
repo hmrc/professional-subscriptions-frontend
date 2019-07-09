f3d7da87c446505a868dff4355b8f3fc0841baf7
Thomas Pascoe <15924950+Pascoe121@users.noreply.github.com>
2019-07-09 15:25:52 +0100

    optimise imports

--- app/forms/mappings/Formatters.scala
+++ app/forms/mappings/Formatters.scala
@@ -16,12 +16,11 @@
 
 package forms.mappings
 
+import models.Enumerable
 import play.api.data.FormError
 import play.api.data.format.Formatter
-import models.Enumerable
 
 import scala.util.control.Exception.nonFatalCatch
-import scala.util.matching.Regex
 
 trait Formatters {
 
