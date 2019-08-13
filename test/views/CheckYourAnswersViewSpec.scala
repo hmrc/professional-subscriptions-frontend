a8eb766afcab8d0255d54709b36274fb766cba94
Adam Liptrot <adam.liptrot@opencastsoftware.com>
2019-08-13 12:05:01 +0100

    Simplified heading nesting

--- test/views/CheckYourAnswersViewSpec.scala
+++ test/views/CheckYourAnswersViewSpec.scala
@@ -31,7 +31,7 @@ class CheckYourAnswersViewSpec extends ViewBehaviours {
 
     val cyaHelper = new CheckYourAnswersHelper(someUserAnswers)
 
-    val sections = Seq(AnswerSection(blockHeadingKey = None, headingClasses = None, headingKey = None, isNested = false, rows = Seq()))
+    val sections = Seq(AnswerSection(headingKey = None, headingClasses = None, subheadingKey = None, rows = Seq()))
 
     val applyView = view.apply(sections)(fakeRequest, messages)
 
