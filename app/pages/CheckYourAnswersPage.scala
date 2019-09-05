1c2117b321b31284fe1cc766d93a7add291eb50f
garethgooch <31101509+garethgooch@users.noreply.github.com>
2019-09-05 13:05:54 +0100

    EE-1053: Update to naviagtion

--- app/pages/CheckYourAnswersPage.scala
+++ app/pages/CheckYourAnswersPage.scala
@@ -14,13 +14,6 @@
  * limitations under the License.
  */
 
-package navigation
+package pages
 
-import play.api.mvc.Call
-import pages._
-import models.{Mode, NormalMode, UserAnswers}
-
-class FakeNavigator(desiredRoute: Call, mode: Mode = NormalMode) extends Navigator {
-  override def nextPage(page: Page, mode: Mode, userAnswers: UserAnswers): Call =
-    desiredRoute
-}
+case object CheckYourAnswersPage extends Page
