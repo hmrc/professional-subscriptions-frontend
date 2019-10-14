c3a822f8de792c003a7c505f47f5c695a201ea69
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2019-10-14 16:28:06 +0100

    EE-968 Remove deprecated code in useranswers.

--- app/models/UserAnswers.scala
+++ app/models/UserAnswers.scala
@@ -32,11 +32,6 @@ final case class UserAnswers(
   def get[A](page: QuestionPage[A])(implicit rds: Reads[A]): Option[A] =
     Reads.optionNoError(Reads.at(page.path)).reads(data).getOrElse(None)
 
-  // TODO: Can remove?
-//  @deprecated("Instead of exposing another API here, we plan to change the API and split the concerns in the QuestionPage into Page, Gettable and Settable.", "0.169.0")
-//  def getByPath[A](path: JsPath)(implicit rds: Reads[A]): Option[A] =
-//    Reads.optionNoError(Reads.at(path)).reads(data).getOrElse(None)
-
   def set[A](page: QuestionPage[A], value: A)(implicit writes: Writes[A]): Try[UserAnswers] = {
 
     val updatedData = data.setObject(page.path, Json.toJson(value)) match {
