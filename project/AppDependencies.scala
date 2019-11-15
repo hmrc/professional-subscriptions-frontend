7fc276f4a9a38d6697d92b7de072f9bef8d1accf
benryan1790 <37295698+benryan1790@users.noreply.github.com>
2019-11-15 15:37:39 +0000

    DL-2474: Add Welsh messages and MessagesSpec

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "org.reactivemongo"     %% "play2-reactivemongo"           % "0.16.0-play26",
+    "org.reactivemongo"     %% "play2-reactivemongo"           % "0.19.0-play26",
     "uk.gov.hmrc"           %% "logback-json-logger"           % "4.6.0",
     "uk.gov.hmrc"           %% "govuk-template"                % "5.45.0-play-26",
     "uk.gov.hmrc"           %% "play-health"                   % "3.14.0-play-26",
@@ -23,7 +23,7 @@ object AppDependencies {
 
   val test: Seq[ModuleID] = Seq(
     "org.scalatest"           %% "scalatest"           % "3.0.8",
-    "org.scalatestplus.play"  %% "scalatestplus-play"  % "2.0.1",
+    "org.scalatestplus.play"  %% "scalatestplus-play"  % "3.1.2",
     "org.pegdown"             %  "pegdown"             % "1.6.0",
     "org.jsoup"               %  "jsoup"               % "1.12.1",
     "com.typesafe.play"       %% "play-test"           % PlayVersion.current,
