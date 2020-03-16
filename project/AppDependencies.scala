e89c815e96d853da275e59bc6094599d4e8c36ae
stephen-oppong-beduh <56881842+stephen-oppong-beduh@users.noreply.github.com>
2020-03-16 11:26:38 +0000

    DL-2862 - Removed 'Cookie failed message authentication check' warning

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,16 +6,16 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"           %% "simple-reactivemongo"          % "7.23.0-play-26",
+    "uk.gov.hmrc"           %% "simple-reactivemongo"          % "7.24.0-play-26",
     "uk.gov.hmrc"           %% "logback-json-logger"           % "4.6.0",
     "uk.gov.hmrc"           %% "govuk-template"                % "5.52.0-play-26",
     "uk.gov.hmrc"           %% "play-ui"                       % "8.8.0-play-26",
     "uk.gov.hmrc"           %% "http-caching-client"           % "9.0.0-play-26",
     "uk.gov.hmrc"           %% "play-conditional-form-mapping" % "1.2.0-play-26",
-    "uk.gov.hmrc"           %% "bootstrap-play-26"             % "1.4.0",
+    "uk.gov.hmrc"           %% "bootstrap-play-26"             % "1.5.0",
     "uk.gov.hmrc"           %% "play-whitelist-filter"         % "3.1.0-play-26",
     "uk.gov.hmrc"           %% "play-partials"                 % "6.9.0-play-26",
-    "org.scalatra.scalate"  %% "play-scalate"                  % "0.5.0",
+    "org.scalatra.scalate"  %% "play-scalate"                  % "0.6.0",
     "org.scalatra.scalate"  %% "scalate-core"                  % "1.9.5",
     "uk.gov.hmrc"           %% "tax-year"                      % "1.0.0"
   )
@@ -28,7 +28,7 @@ object AppDependencies {
     "com.typesafe.play"       %% "play-test"           % PlayVersion.current,
     "org.mockito"             %  "mockito-all"         % "1.10.19",
     "org.scalacheck"          %% "scalacheck"          % "1.14.3",
-    "com.github.tomakehurst"  %  "wiremock-standalone" % "2.26.0"
+    "com.github.tomakehurst"  %  "wiremock-standalone" % "2.26.3"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
