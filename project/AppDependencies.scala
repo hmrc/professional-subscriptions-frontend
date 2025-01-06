25a8e1cd375db0646165ed944a1279e7c0c3b7c2
upasanasingh-hmrc <175723122+upasanasingh-hmrc@users.noreply.github.com>
2025-01-06 15:10:13 +0000

    DL-15074 Dependency changes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,15 +2,15 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.1.0"
-  val mongoVersion = "2.2.0"
+  val bootstrapVersion = "9.6.0"
+  val mongoVersion = "2.3.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"                     % mongoVersion,
-    "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30"  % "3.0.0",
+    "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30"  % "3.2.0",
     "uk.gov.hmrc"           %% "tax-year"                               % "5.0.0",
-    "uk.gov.hmrc"           %% "sca-wrapper-play-30"                    % "1.10.0"
+    "uk.gov.hmrc"           %% "sca-wrapper-play-30"                    % "2.1.0"
   )
 
   val test: Seq[ModuleID] = Seq(
